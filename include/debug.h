0c574581bf4b6d6dcde76f27161cc017d4ee97dc
Ben Jacobs <ben.jacobs@vanti.co.uk>
2020-05-02 13:49:45 +0100

    feat: disable debugging

--- include/debug.h
+++ include/debug.h
@@ -1,3 +1,5 @@
-#define debugPrint(a) (Serial.print(a))
-#define debugPrintln(a) (Serial.println(a))
+// #define debugPrint(a) (Serial.print(a))
+#define debugPrint(a)
+// #define debugPrintln(a) (Serial.println(a))
+#define debugPrintln(a)
 #define debug false
